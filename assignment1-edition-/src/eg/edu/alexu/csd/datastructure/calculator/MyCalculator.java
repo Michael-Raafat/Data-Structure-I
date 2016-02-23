4bce9a75226ab2f6f919575ae7f070f0a2911393
Michael Raafat <mico.raafat96@gmail.com>
2016-02-23 20:19:52 +0200

    another try

--- assignment1-edition-/src/eg/edu/alexu/csd/datastructure/calculator/MyCalculator.java
+++ assignment1-edition-/src/eg/edu/alexu/csd/datastructure/calculator/MyCalculator.java
@@ -1,5 +1,14 @@
 package eg.edu.alexu.csd.datastructure.calculator;
 
-public class MyCalculator {
+public class MyCalculator implements ICalculator {
+
+	public int add(int x, int y) {
+		
+		return x+y;
+	}
+
+	public float divide(int x, int y) {
+		return (float)x/y;
+	}
 
 }
--- assignment1/src/eg/edu/alexu/csd/datastructure/calculator/cs59/ICalculator.java
+++
@@ -1,8 +0,0 @@
-package eg.edu.alexu.csd.datastructure.calculator.cs59;
-
-public interface ICalculator {
-	int add(int x, int y);
-	float divide(int x, int y);
-
-
-}

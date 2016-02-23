494255ea5bb2743a83b913363467103173aeb102
Michael Raafat <mico.raafat96@gmail.com>
2016-02-23 20:16:16 +0200

    another code

---
+++ assignment1-edition-/src/eg/edu/alexu/csd/datastructure/calculator/ICalculator.java
@@ -0,0 +1,8 @@
+package eg.edu.alexu.csd.datastructure.calculator;
+
+public interface ICalculator {
+	int add(int x, int y);
+	float divide(int x, int y);
+
+
+}
---
+++ assignment1-edition-/src/eg/edu/alexu/csd/datastructure/calculator/MyCalculator.java
@@ -0,0 +1,5 @@
+package eg.edu.alexu.csd.datastructure.calculator;
+
+public class MyCalculator {
+
+}

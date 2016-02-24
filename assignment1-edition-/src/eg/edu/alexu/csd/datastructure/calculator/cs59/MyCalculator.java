a1f437151320bb63796ccde1279b4116047424d5
Michael Raafat <mico.raafat96@gmail.com>
2016-02-24 17:09:58 +0200

    new version

---
+++ assignment1-edition-/src/eg/edu/alexu/csd/datastructure/calculator/cs59/MyCalculator.java
@@ -0,0 +1,19 @@
+package eg.edu.alexu.csd.datastructure.calculator.cs59;
+
+import eg.edu.alexu.csd.datastructure.calculator.ICalculator;
+
+public class MyCalculator implements ICalculator {
+
+	@Override
+	public int add(int x, int y) {
+		// TODO Auto-generated method stub
+		return x+y;
+	}
+
+	@Override
+	public float divide(int x, int y) {
+		// TODO Auto-generated method stub
+		return (float)x/y;
+	}
+
+}

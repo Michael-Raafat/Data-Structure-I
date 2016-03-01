163a4898a784f853d2d4c0a559d38121c9b1cbe5
Michael Raafat <mico.raafat96@gmail.com>
2016-03-01 19:03:17 +0200

    eight

--- assignment2/src/eg/edu/alexu/csd/datastructure/hangman/cs59/HangMan.java
+++ assignment2/src/eg/edu/alexu/csd/datastructure/hangman/cs59/HangMan.java
@@ -1,16 +1,19 @@
 
 package eg.edu.alexu.csd.datastructure.hangman.cs59;
 import java.util.Random;
+import java.io.*;
+import java.lang.*;
 import eg.edu.alexu.csd.datastructure.hangman.IHangman;
 
 
 public class HangMan implements IHangman {
 
 	String[] dictionary = new String [1000];
-	int maximum,y=0;
+	int maximum;
 	int j,m;
 	char[] a = new char [1000];
-
+	char[] n = new char [1000];
+	StringBuilder f = new StringBuilder("") ;
 	public void setDictionary(String[] words) {
 		int i ;
 		j=words.length;
@@ -40,9 +43,10 @@ public class HangMan implements IHangman {
 		{	 
 			return "";
 		}
-		int k =0 ;
+		int k =0 , l=0;
 		char[] b = dictionary[m].toUpperCase().toCharArray();
 		String r = String.copyValueOf(a, 0, dictionary[m].length());
+		String n = f.toString().toUpperCase() ;
 		if (c== null)
 		{
 			return r;
@@ -63,9 +67,23 @@ public class HangMan implements IHangman {
 		}
 		else
 		{
-			y++;
+			if(k==0)
+			{
+				for(int j=0;j<n.length();j++)
+				{
+					if(n.charAt(j)== c)
+					{
+						l=1;
+					}
+				}
+				if(l==0)
+					{
+					   f=f.append(c);
+					   maximum--;
+					}
+			}
 		}
-		if (y> maximum)
+		if (maximum < 0)
 		{
 			return null ;
 		}

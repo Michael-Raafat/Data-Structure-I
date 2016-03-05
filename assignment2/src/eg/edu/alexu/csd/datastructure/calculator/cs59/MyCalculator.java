076713f5bc5fdf2ba6223d2afeb3034d956dc3ba
Michael Raafat <mico.raafat96@gmail.com>
2016-03-05 13:26:16 +0200

    tmam awii

--- assignment2/src/eg/edu/alexu/csd/datastructure/calculator/cs59/MyCalculator.java
+++ assignment2/src/eg/edu/alexu/csd/datastructure/calculator/cs59/MyCalculator.java
@@ -1,19 +1,19 @@
-package ed.edu.alexu.csd.datastructure.calculator.cs59;
+package eg.edu.alexu.csd.datastructure.calculator.cs59;
 
 import eg.edu.alexu.csd.datastructure.calculator.ICalculator;
 
 public class MyCalculator implements ICalculator {
 
-	@Override
+	
 	public int add(int x, int y) {
-		
 		return x+y;
 	}
-
-	@Override
+	
 	public float divide(int x, int y) {
-		
-		return (float)x/y;
+			return (float)x/y ;
 	}
+	
+	
+	
 
 }

1d8ba91b50c2c044ef94a834afbf6c30a5cb3dfb
Michael <micoraafat96@gmail.com>
2016-04-13 16:58:42 -0700

    ha kda

--- assignment2/src/eg/edu/alexu/csd/datastructure/iceHockey/cs59/MyIceHockey.java
+++ assignment2/src/eg/edu/alexu/csd/datastructure/iceHockey/cs59/MyIceHockey.java
@@ -3,16 +3,32 @@ package eg.edu.alexu.csd.datastructure.iceHockey.cs59;
 import java.awt.Point;
 
 import eg.edu.alexu.csd.datastructure.iceHockey.IPlayersFinder;
-
+/**
+ *
+ * @author Michael.
+ *
+ */
 public class MyIceHockey implements IPlayersFinder {
+	/**
+	 * array 2d to store picture.
+	 */
 	char[][] a = new char[100][100];
+	/**
+	 * array of points.
+	 */
 	Point[] it = new Point[2500];
-	int down = 0, zew = 1, up = 0, left = 0, right = 0;
-	int t = -1, t2 = -1;
-	int p = -1, ioo = 0, ioo1 = 0, foo = 0, joo = 0, joo1 = 0, joo2 = 0;
-	int f = 0;
-
-	public void settingarray(String[] photo) {
+	/**
+	 * many variables.
+	 */
+	int down = 0, zew = 1, up = 0, left = 0, right = 0,
+			t = -1, t2 = -1, p = -1, ioo = 0, ioo1 = 0,
+			foo = 0, joo = 0, joo1 = 0, joo2 = 0, f = 0;
+    /**
+     * to set the array.
+     * @param photo
+     * the picture.
+     */
+	public final void settingarray(final String[] photo) {
 		for (int i = 0; i < photo.length; i++) {
 			for (int j = 0; j < photo[i].length(); j++) {
 				a[i][j] = photo[i].charAt(j);
@@ -20,8 +36,12 @@ public class MyIceHockey implements IPlayersFinder {
 		}
 
 	}
-
-	public void arraysort(Point[] real) {
+    /**
+     * to sort array of points.
+     * @param real
+     * array of points.
+     */
+	public final void arraysort(final Point[] real) {
 		Point temp = new java.awt.Point(1000, 1000);
 		if (real.length >= 0 && real[0] != null) {
 			for (int i = 0; i < real.length - 1; i++) {
@@ -46,22 +66,27 @@ public class MyIceHockey implements IPlayersFinder {
 		}
 
 	}
-
-	public Point[] findPlayers(String[] photo, int team, int threshold) {
+    @Override
+	public final Point[] findPlayers(
+			final String[] photo, final int team,
+			final int threshold) {
 		settingarray(photo);
 		for (int i = 0; i < photo.length; i++) {
 			for (int j = 0; j < photo[i].length(); j++) {
 				foo = i;
 				ioo = i;
 				ioo1 = i;
-				if (a[i][j] == String.valueOf(team).toCharArray()[0]) {
+				if (a[i][j]
+						==
+					String.valueOf(team).toCharArray()[0]) {
 
 					joo = j;
 					joo1 = j;
 					joo2 = j;
-					Point hhh = centre(i, j, a, threshold, photo[i].length(), team);
-					if (hhh == null)
-						;
+					Point hhh = centre(
+						i, j, a, threshold,
+						photo[i].length(), team);
+					if (hhh == null) { }
 					else {
 						it[f] = hhh;
 						f++;
@@ -73,28 +98,52 @@ public class MyIceHockey implements IPlayersFinder {
 		for (int i = 0; i < f; i++) {
 			real[i] = it[i];
 		}
-		if (f != 0)
+		if (f != 0) {
 			arraysort(real);
+		}
 		f = 0;
 		return real;
 	}
-
-	public Point centre(int i, int j, char[][] s, int threshold, int length, int team) {
+   /**
+     * recursive method
+     * @param i
+     * x.
+     * @param j
+     * y.
+     * @param s
+     * array 2d.
+     * @param threshold
+     * area.
+     * @param length
+     * size.
+     * @param team
+     * number.
+     * @return point.
+     */
+	public final Point centre(final int i, final int j,
+			final char[][] s, final int threshold,
+			final int length, final int team) {
 		s[i][j] = '-';
-		if (i + 1 < s.length && s[i + 1][j] == String.valueOf(team).toCharArray()[0]) {
-			if (i + 1 < s.length)
+		if (i + 1 < s.length && s[i + 1][j]
+				==
+				String.valueOf(team).toCharArray()[0]) {
+			if (i + 1 < s.length) {
 				s[i + 1][j] = '-';
+			}
 			zew++;
 			if (i + 1 > ioo1) {
 				down++;
 				ioo1 = i + 1;
 			}
-			if (i + 1 < s.length)
+			if (i + 1 < s.length) {
 				centre(i + 1, j, a, threshold, length, team);
+			}
 		}
-		if (j + 1 < length && s[i][j + 1] == String.valueOf(team).toCharArray()[0]) {
-			if (j + 1 < s.length)
+		if (j + 1 < length && s[i][j + 1]
+				== String.valueOf(team).toCharArray()[0]) {
+			if (j + 1 < s.length) {
 				s[i][j + 1] = '-';
+			}
 			zew++;
 			if (j + 1 > joo1) {
 				right++;
@@ -102,38 +151,51 @@ public class MyIceHockey implements IPlayersFinder {
 			}
 			centre(i, j + 1, a, threshold, length, team);
 		}
-		if (j - 1 >= 0 && s[i][j - 1] == String.valueOf(team).toCharArray()[0]) {
-			if (j - 1 >= 0)
+		if (j - 1 >= 0 && s[i][j - 1]
+				==
+				String.valueOf(team).toCharArray()[0]) {
+			if (j - 1 >= 0) {
 				s[i][j - 1] = '-';
+			}
 			zew++;
 			if (j - 1 < joo2) {
 				left++;
 				joo2 = j - 1;
 			}
-			if (j - 1 >= 0)
+			if (j - 1 >= 0) {
 				centre(i, j - 1, a, threshold, length, team);
+			}
 		}
-		if (i - 1 >= 0 && s[i - 1][j] == String.valueOf(team).toCharArray()[0]) {
-			if (i >= 0)
+		if (i - 1 >= 0 && s[i - 1][j]
+				==
+				String.valueOf(team).toCharArray()[0]) {
+			if (i >= 0) {
 				s[i - 1][j] = '-';
+			}
 			zew++;
 			if (i - 1 < ioo) {
 				up++;
 				ioo = i - 1;
 			}
-			if (i - 1 >= 0)
+			if (i - 1 >= 0) {
 				centre(i - 1, j, a, threshold, length, team);
+			}
 
 		}
 		if (foo == i && joo == j) {
 			if ((4 * zew) >= threshold) {
+				int centrex = 0, centrey = 0;
 				int y = up + down + 1;
 				int x = left + right + 1;
 				if (j > joo2) {
-					j = joo2;
+					centrex = x + joo2 * 2;
+				} else {
+					centrex = x + j * 2;
 				}
 				if (i > ioo) {
-					i = ioo;
+					centrey = y + ioo * 2;
+				} else {
+					centrey = y + i * 2;
 				}
 				up = 0;
 				down = 0;
@@ -146,10 +208,8 @@ public class MyIceHockey implements IPlayersFinder {
 				ioo = 0;
 				t2 = -1;
 				p = -1;
-
-				int centrex = x + j * 2;
-				int centrey = y + i * 2;
-				Point point = new java.awt.Point(centrex, centrey);
+				Point point =
+				new java.awt.Point(centrex, centrey);
 
 				return point;
 			} else {
@@ -168,8 +228,9 @@ public class MyIceHockey implements IPlayersFinder {
 				Point point = null;
 				return point;
 			}
-		} else
+		} else {
 			return null;
+		}
 	}
 
 }
--- assignment2/src/eg/edu/alexu/csd/datastructure/iceHockey/cs59/testTest.java
+++ assignment2/src/eg/edu/alexu/csd/datastructure/iceHockey/cs59/testTest.java
@@ -2,51 +2,89 @@ package eg.edu.alexu.csd.datastructure.iceHockey.cs59;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+/**
+ * 
+ * @author Michael.
+ *
+ */
 public class testTest {
 	@Test
-	public void testCorner() {
+	public final void testCorner() {
 		MyIceHockey pl = new MyIceHockey();
-		java.awt.Point[] points = { new java.awt.Point(1, 6), new java.awt.Point(13, 6) };
-		String[] photo = { "4QW4QW4", "T4REW4Q", "4Q4W4E4", "4ER4TY4" };
-		Assert.assertArrayEquals("test1", points, pl.findPlayers(photo, 4, 5));
+		java.awt.Point[] points = {
+				new java.awt.Point(1, 6),
+				new java.awt.Point(13, 6) };
+		String[] photo = {"4QW4QW4",
+				"T4REW4Q",
+				"4Q4W4E4",
+				"4ER4TY4" };
+		Assert.assertArrayEquals("test1", points,
+				pl.findPlayers(photo, 4, 5));
 	}
 
 	@Test
-	public void testDirections() {
+	public final void testDirections() {
 		MyIceHockey pl = new MyIceHockey();
 		java.awt.Point[] points = {};
-		String[] photo = { "1J1U1B1U1", "R1U1R1E1W", "1P1R1N1B1", "P1O1N1S1D", "1E1E1E1E1" };
-		Assert.assertArrayEquals("test2", points, pl.findPlayers(photo, 1, 5));
+		String[] photo = {"1J1U1B1U1",
+				"R1U1R1E1W",
+				"1P1R1N1B1",
+				"P1O1N1S1D",
+				"1E1E1E1E1" };
+		Assert.assertArrayEquals("test2", points,
+				pl.findPlayers(photo, 1, 5));
 	}
 
 	@Test
-	public void testMonster() {
+	public final void testMonster() {
 		MyIceHockey pl = new MyIceHockey();
-		java.awt.Point[] points = { new java.awt.Point(13, 8) };
-		String[] photo = { "8888888888888", "8888888888888", "8888888888888", "8888888888888", "8QJ88R8SG88TY",
-				"88V88B88OUZ8O", "88WF8Q8GG88B8", "8MX88D88888T8K8" };
-		Assert.assertArrayEquals("test3", points, pl.findPlayers(photo, 8, 9));
+		java.awt.Point[] points = {
+				new java.awt.Point(13, 8) };
+		String[] photo = {"8888888888888",
+				"8888888888888",
+				"8888888888888",
+				"8888888888888",
+				"8QJ88R8SG88TY",
+				"88V88B88OUZ8O",
+				"88WF8Q8GG88B8",
+				"8MX88D88888T8K8" };
+		Assert.assertArrayEquals("test3", points,
+				pl.findPlayers(photo, 8, 9));
 	}
 
 	// 3
 	@Test
-	public void test4() {
+	public final void test4() {
 		MyIceHockey pl = new MyIceHockey();
-		java.awt.Point[] points = { new java.awt.Point(8, 2), new java.awt.Point(9, 8) };
-		String[] photo = { "44K44L444", "44J44T4X4", "44OEVI4F4", "4444444D4", "4VY4KBYM4", "4EW4H4ER4", "4U4ZDUS4",
+		java.awt.Point[] points = { new java.awt.Point(8, 2),
+				new java.awt.Point(9, 8) };
+		String[] photo = {"44K44L444",
+				"44J44T4X4",
+				"44OEVI4F4",
+				"4444444D4",
+				"4VY4KBYM4",
+				"4EW4H4ER4",
+				"4U4ZDUS4",
 				"444444444" };
-		Assert.assertArrayEquals("test4", points, pl.findPlayers(photo, 4, 12));
+		Assert.assertArrayEquals("test4", points,
+				pl.findPlayers(photo, 4, 12));
 
 	}
 
 	@Test
-	public void testBigMan() {
+	public final void testBigMan() {
 		MyIceHockey pl = new MyIceHockey();
-		java.awt.Point[] points = { new java.awt.Point(9, 8) };
-		String[] photo = { "33KEEL333", "33JERT3X3", "33OEVI3F3", "3333333D3", "3VY3KBYM3", "3EW3H4ER3", "3UR4DUS3",
+		java.awt.Point[] points = {new java.awt.Point(9, 8) };
+		String[] photo = {"33KEEL333",
+				"33JERT3X3",
+				"33OEVI3F3",
+				"3333333D3",
+				"3VY3KBYM3",
+				"3EW3H4ER3",
+				"3UR4DUS3",
 				"333333333" };
-		Assert.assertArrayEquals("test5", points, pl.findPlayers(photo, 3, 3));
+		Assert.assertArrayEquals("test5", points,
+				pl.findPlayers(photo, 3, 3));
 
 	}
 }

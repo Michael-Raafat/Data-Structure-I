c20e115b2c8be825b1a57963d0b3695890070ceb
Michael <micoraafat96@gmail.com>
2016-04-14 14:55:26 -0700

    yarab

--- assignment2/src/eg/edu/alexu/csd/datastructure/hangman/cs59/HangMan.java
+++ assignment2/src/eg/edu/alexu/csd/datastructure/hangman/cs59/HangMan.java
@@ -1,102 +1,112 @@
-
-
-
-
-
-
 package eg.edu.alexu.csd.datastructure.hangman.cs59;
 
 import java.util.Random;
-import java.io.*;
-import java.lang.*;
 import eg.edu.alexu.csd.datastructure.hangman.IHangman;
+/**
+ * hangman game.
+ * @author Michael
+ *
+ */
 
 public class HangMan implements IHangman {
-
-	String[] dictionary = new String[1000];
-	int maximum;
-	int j, m;
-	char[] a = new char[1000];
-	char[] n = new char[1000];
-	StringBuilder f = new StringBuilder("");
-	StringBuilder p = new StringBuilder("");
-
-	public void setDictionary(String[] words) {
-		int i;
-		j = words.length;
-		for (i = 0; i < words.length; i++) {
-			dictionary[i] = words[i];
-		}
-	}
-
-	public String selectRandomSecretWord() {
-		Random word = new Random();
-		if (j == 0)
-			return null;
-		int right = word.nextInt(j);
-		m = right;
-		for (int i = 0; i < dictionary[right].length(); i++) {
-			if (dictionary[right].charAt(i) != ' ')
-				a[i] = '-';
-		}
-		return dictionary[right];
-
-	}
-
-	public String guess(Character c) {
-		if (j == 0) {
-			return "";
-		}
-		int k = 0, e = 0, w = 0;
-		char[] b = dictionary[m].toUpperCase().toCharArray();
-		String r = String.copyValueOf(a, 0, dictionary[m].length());
-		if (c == null) {
-			return r;
-		}
-		for (int i = 0; i < b.length; i++) {
-			if (Character.toUpperCase(c) == b[i]) {
-				if (e == 0) {
-					p = p.append(c);
-					e = 1;
-				}
-				a[i] = dictionary[m].charAt(i);
-				k = 1;
-			}
-		}
-		r = String.copyValueOf(a, 0, dictionary[m].length());
-		if (k == 1) {
-			return r;
-		} else {
-			if (k == 0) {
-				for (int i = 0; i < dictionary[m].length(); i++) {
-					for (int j = 0; j < p.length(); j++) {
-						if (b[i] == p.charAt(j)) {
-							a[i] = dictionary[m].charAt(i);
-							w = 1;
-						}
-					}
-					if (w == 0) {
-						a[i] = '-';
-					}
-				}
-				r = String.copyValueOf(a, 0, dictionary[m].length());
-				maximum--;
-			}
-		}
-		if (maximum <= 0) {
-			return null;
-		} else
-			return r;
-
-	}
-
-	public void setMaxWrongGuesses(Integer max) {
-		if (max == null) {
-			maximum = 0;
-		} else {
-			maximum = max;
-		}
-
-	}
-
+    /**
+     * dictionary String.
+     */
+    private String[] dictionary = new String[1000];
+    /**
+     * maximum guesses.
+     */
+    private int maximum;
+    /**
+     * variables for switching.
+     */
+    private int j, m;
+    /**
+     * char array to use in converting from.
+     * string to array
+     */
+    private char[] a = new char[1000];
+    /**
+     * string builder to append true guess.
+     */
+    private StringBuilder p = new StringBuilder("");
+    @Override
+    public final void setDictionary(final String[] words) {
+        int i;
+        j = words.length;
+        for (i = 0; i < words.length; i++) {
+             dictionary[i] = words[i];
+        }
+    }
+    @Override
+    public final String selectRandomSecretWord() {
+        Random word = new Random();
+        if (j == 0) {
+           return null;
+        }
+        int right = word.nextInt(j);
+        m = right;
+        for (int i = 0; i < dictionary[right].length(); i++) {
+             if (dictionary[right].charAt(i) != ' ') {
+                 a[i] = '-';
+            }
+        }
+        return dictionary[right];
+    }
+    @Override
+    public final String guess(final Character c) {
+        if (j == 0) {
+           return "";
+        }
+        int k = 0, e = 0, w = 0;
+        char[] b = dictionary[m].toUpperCase().toCharArray();
+        String r = String.copyValueOf(a, 0, dictionary[m].length());
+        if (c == null) {
+           return r;
+        }
+        for (int i = 0; i < b.length; i++) {
+           if (Character.toUpperCase(c) == b[i]) {
+                if (e == 0) {
+                    p = p.append(c);
+                    e = 1;
+                }
+                a[i] = dictionary[m].charAt(i);
+                k = 1;
+            }
+        }
+        r = String.copyValueOf(a, 0, dictionary[m].length());
+        if (k == 1) {
+            return r;
+            } else {
+               if (k == 0) {
+               for (int i = 0; i < dictionary[m].length(); i++) {
+                 for (int y = 0; y < p.length(); y++) {
+                        if (b[i] == p.charAt(y)) {
+                            a[i] = dictionary[m].charAt(i);
+                            w = 1;
+                            }
+                 }
+                 if (w == 0) {
+                    a[i] = '-';
+                    }
+                }
+                 r = String.copyValueOf(
+                 a, 0, dictionary[m].length());
+                 maximum--;
+                }
+               }
+             if (maximum <= 0) {
+                return null;
+              } else {
+                return r;
+                 }
+             }
+    @Override
+    public final void setMaxWrongGuesses(final Integer max) {
+        if (max == null) {
+            maximum = 0;
+            } else {
+               maximum = max;
+             }
+          }
 }

90ef4d743eac4329934fe417b4bb373e50be7c4a
Michael <micoraafat96@gmail.com>
2016-04-15 10:40:22 +0200

    kda tmam Mystack

--- assignment2/src/eg/edu/alexu/csd/datastructure/stack/cs59/MyStack.java
+++ assignment2/src/eg/edu/alexu/csd/datastructure/stack/cs59/MyStack.java
@@ -2,7 +2,7 @@ package eg.edu.alexu.csd.datastructure.stack.cs59;
 
 import eg.edu.alexu.csd.datastructure.stack.IStack;
 /**
- * 
+ *
  * @author Michael.
  *
  */
@@ -16,9 +16,28 @@ public class MyStack implements IStack {
 	 */
 	private Node top = new Node();
 	@Override
-	public void add(int index, Object element) {
+	public void add(final int index, final Object element) {
 		// TODO Auto-generated method stub
-		
+		if(index > size || index < 0) {
+			throw new RuntimeException("ERROR");
+		}
+		Node v = top;
+		Node temp = new Node(element, null);
+		if(index == size && size != 0 ) {
+			temp.setNext(top);
+			top = temp;
+		}
+		else if (size == 0 && index == size ) {
+			top.setElement(element);
+		}
+		else {
+	        	for(int i = size-1; i > index+1; i--) {
+		     	v = v.getNext();
+	         	}
+		        temp.setNext(v.getNext());
+		        v.setNext(temp);
+		        size++;
+		}
 	}
 
 	@Override

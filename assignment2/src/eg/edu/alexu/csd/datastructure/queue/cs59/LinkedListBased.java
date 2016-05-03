19d0050f49a8a7b6c4e8d3e3161afb564ee1f7b3
Michael <micoraafat96@gmail.com>
2016-05-04 00:15:48 +0300

    jkj

--- assignment2/src/eg/edu/alexu/csd/datastructure/queue/cs59/LinkedListBased.java
+++ assignment2/src/eg/edu/alexu/csd/datastructure/queue/cs59/LinkedListBased.java
@@ -21,12 +21,12 @@ public class LinkedListBased implements IQueue, ILinkedBased {
 	 */
 	private Node tail = new Node();
 	@Override
-	public void enqueue(Object item) {
+	public void enqueue(final Object item) {
 		// TODO Auto-generated method stub
 		if (item.equals(null)) {
 			throw new RuntimeException();
-		}        	
-		Node temp = new Node (item);
+		}
+		Node temp = new Node(item);
 					if (isEmpty()) {
 						head = temp;
 						tail = temp;
@@ -46,7 +46,6 @@ public class LinkedListBased implements IQueue, ILinkedBased {
 		}
 		Object q = head.getElement();
 		head = head.getNext();
-		//head.setPrev(null);
 		size--;
 		if (isEmpty()) {
 			tail = new Node();
@@ -65,66 +64,4 @@ public class LinkedListBased implements IQueue, ILinkedBased {
 		// TODO Auto-generated method stub
 		return size;
 	}
-/*
-	/**
-     * list size.
-     *//*
-	private int size = 0;
-	/**
-	 * list head.
-	 *//*
-	private Node head = new Node();
-	/**
-	 * list tail.
-	 *//*
-	private Node tail = new Node();
-	@Override
-	public void enqueue(final Object item) {
-		// TODO Auto-generated method stub
-		if (item.equals(null)) {
-			throw new RuntimeException();
-		}        	
-		Node temp = new Node (item);
-					if (isEmpty()) {
-						head = temp;
-						tail = temp;
-		    		} else {
-						tail.setNext(temp);
-						temp.setPrev(tail);
-						tail = temp;
-					}
-		 			size++;
-	}
-
-	@Override
-	public Object dequeue() {
-		// TODO Auto-generated method stub
-		if (isEmpty()) {
-			throw new RuntimeException("ERROR");
-		}
-		Object q = head.getElement();
-		head = head.getNext();
-		head.setPrev(null);
-		size--;
-		if (isEmpty()) {
-			tail = new Node();
-		}
-		return q;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		if (size == 0) {
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return size;
-	}
-*/
 }

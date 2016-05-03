4903991c6bd6bffdd22e0af32611d60990580db5
Michael <micoraafat96@gmail.com>
2016-05-03 21:56:34 +0300

    h

--- assignment2/src/eg/edu/alexu/csd/datastructure/queue/cs59/LinkedListBased.java
+++ assignment2/src/eg/edu/alexu/csd/datastructure/queue/cs59/LinkedListBased.java
@@ -8,6 +8,30 @@ import eg.edu.alexu.csd.datastructure.queue.IQueue;
  */
 
 public class LinkedListBased implements IQueue, ILinkedBased {
+
+	@Override
+	public void enqueue(Object item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 /*
 	/**
      * list size.

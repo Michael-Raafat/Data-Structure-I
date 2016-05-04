b0342a45d4671ccb75c2b3fcaf0311d2f2619064
Michael <micoraafat96@gmail.com>
2016-05-04 22:24:44 +0300

    3aa

---
+++ assignment2/src/eg/edu/alexu/csd/datastructure/maze/cs59/MazeSolver.java
@@ -0,0 +1,21 @@
+package eg.edu.alexu.csd.datastructure.maze.cs59;
+
+import java.io.File;
+
+import eg.edu.alexu.csd.datastructure.maze.IMazeSolver;
+
+public class MazeSolver implements IMazeSolver {
+
+	@Override
+	public int[][] solveBFS(File maze) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[][] solveDFS(File maze) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

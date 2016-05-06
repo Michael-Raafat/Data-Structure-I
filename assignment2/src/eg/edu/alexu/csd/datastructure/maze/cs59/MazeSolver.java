89428d5e4bf9523d781098bd6a2e39e685c954ca
Michael <micoraafat96@gmail.com>
2016-05-06 13:31:40 +0300

    ha

--- assignment2/src/eg/edu/alexu/csd/datastructure/maze/cs59/MazeSolver.java
+++ assignment2/src/eg/edu/alexu/csd/datastructure/maze/cs59/MazeSolver.java
@@ -18,6 +18,7 @@ public class MazeSolver implements IMazeSolver {
 	public int[][] solveBFS(final File maze) {
 		// TODO Auto-generated method stub
 		LinkedListBased q;
+		StringBuilder h = new StringBuilder();
 		try {
 			String zew;
 			Scanner s = new Scanner(maze);
@@ -57,7 +58,21 @@ public class MazeSolver implements IMazeSolver {
 					}
 				}
 			}
-			
+			if (s.hasNext()) {
+				zew = s.nextLine();
+				for (int j = 0; j < n; j++) {
+					for (int k = 0; k < m; k++) {
+						h.append(zew.charAt(k));
+						if (k == m - 1 && zew.charAt(k+1) != ' ') {
+							throw new RuntimeException();
+						}
+					}
+					zew = s.nextLine();
+					if (j == n - 1 && s.hasNext()) {
+						throw new RuntimeException();
+					}
+				}
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -79,6 +94,65 @@ public class MazeSolver implements IMazeSolver {
 	@Override
 	public int[][] solveDFS(final File maze) {
 		// TODO Auto-generated method stub
+		StringBuilder h = new StringBuilder();
+		try {
+			String zew;
+			Scanner s = new Scanner(maze);
+			if (s.hasNext()) {
+				zew = s.nextLine();
+				for(int i = 0; i < zew.length(); i++) {
+					if (n == 0) {
+						if (isNumber(zew.charAt(i))) {
+							n += Integer.valueOf(
+			 						String.valueOf(
+			 		 				zew.charAt(i)));
+						while (isNumber(zew.charAt(i+1))) {
+								i++;
+								n *= 10;
+								n += Integer.valueOf(
+				 						String.valueOf(
+				 		 				zew.charAt(i)));
+								
+							}
+						}	
+					} else if (m == 0) {
+						if (isNumber(zew.charAt(i))) {
+							m += Integer.valueOf(
+			 						String.valueOf(
+			 		 				zew.charAt(i)));
+						while (isNumber(zew.charAt(i+1))) {
+								i++;
+								m *= 10;
+								m += Integer.valueOf(
+				 						String.valueOf(
+				 		 				zew.charAt(i)));
+								
+							}
+						}	
+					} else if (zew.charAt(i) != ' ') {
+						throw new RuntimeException();
+					}
+				}
+			}
+			if (s.hasNext()) {
+				zew = s.nextLine();
+				for (int j = 0; j < n; j++) {
+					for (int k = 0; k < m; k++) {
+						h.append(zew.charAt(k));
+						if (k == m - 1 && zew.charAt(k+1) != ' ') {
+							throw new RuntimeException();
+						}
+					}
+					zew = s.nextLine();
+					if (j == n - 1 && s.hasNext()) {
+						throw new RuntimeException();
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 

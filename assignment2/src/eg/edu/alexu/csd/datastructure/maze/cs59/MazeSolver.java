c453e06a4fbed8d1b93133b2769314eb9fad699b
Michael <micoraafat96@gmail.com>
2016-05-08 02:42:00 +0300

    s

--- assignment2/src/eg/edu/alexu/csd/datastructure/maze/cs59/MazeSolver.java
+++ assignment2/src/eg/edu/alexu/csd/datastructure/maze/cs59/MazeSolver.java
@@ -33,26 +33,21 @@ public class MazeSolver implements IMazeSolver {
 			if (s.hasNext()) {
 				n = s.nextInt();
 				m = s.nextInt();
-			}	
+           }
 			if (s.hasNext()) {
 				int you = 0, me = 0;
 				for (int j = 0; j < n; j++) {
 					zew = s.next();
-					int vv = 0;
 					for (int k = 0; k < m; k++) {
 						h.append(zew.charAt(k));
 						if (Character.valueOf(
-							zew.charAt(k)).equals('S')) {
+					 zew.charAt(k)).equals('S')) {
 							you++;
-						} else if (Character.valueOf(
-								zew.charAt(k)).equals('E')) {
-							me = 1;
-						} 
-						vv = k + 1;
+					} else if (Character.valueOf(
+						zew.charAt(k)).equals('E')) {
+						me = 1;
 					}
-					if (vv < zew.length()) {
-						throw new RuntimeException();
-					}
+				}
 				}
 				if (me == 0 || you > 1) {
 					throw new RuntimeException();
@@ -61,10 +56,9 @@ public class MazeSolver implements IMazeSolver {
 				throw new RuntimeException();
 			}
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			throw new RuntimeException();
 		}
-		Node [][] f = new Node[n][m];
+		Node[][] f = new Node[n][m];
 		int b = 0;
 		for (int y = 0; y < n; y++) {
 			for (int z = 0; z < m; z++) {
@@ -75,7 +69,7 @@ public class MazeSolver implements IMazeSolver {
 			}
 		}
 		int fx = 0, fy = 0, kaza = 0;
-		boolean d = true, p = true, gg = true;
+		boolean d = true, p = true;
 		for (int t = 0; t < n; t++) {
 			for (int r = 0; r < m; r++) {
 				if (Character.valueOf((char)
@@ -90,76 +84,66 @@ public class MazeSolver implements IMazeSolver {
 					    int x = Integer.valueOf(
 					    String.valueOf(temp.getx()));
 					    int y = Integer.valueOf(
-							    String.valueOf(temp.gety()));
+						String.valueOf(temp.gety()));
 					    if (String.valueOf(
-								f[x][y].getVal(
-								)).toCharArray(
-									)[0] == 'E') {
+		    f[x][y].getVal()).toCharArray()[0] == 'E') {
 					    	p = false;
 					    	fx = Integer.valueOf(
-					    	String.valueOf(f[x][y].getx()));
-					    	fy = Integer.valueOf(
-							    	String.valueOf(f[x][y].gety()));
+					   	String.valueOf(f[x][y].getx()));
+					  fy = Integer.valueOf(
+					String.valueOf(f[x][y].gety()));
 					    	break;
 					    }
 					    if (x - 1 >= 0
-								&& (String.valueOf(
-								f[x - 1][y].getVal(
-								)).toCharArray()[0] == '.'
-								|| String.valueOf(
-										f[x - 1][y].getVal(
-										)).toCharArray(
-										)[0] == 'E')
-								&& !f[x - 1][y].getVisited()) {
-							f[x - 1][y].setParent(f[x][y]);
+				&& (String.valueOf(f[x - 1][y].getVal(
+						)).toCharArray()[0] == '.'
+							|| String.valueOf(
+							f[x - 1][y].
+							getVal()).toCharArray(
+							)[0] == 'E')
+					&& !f[x - 1][y].getVisited()) {
+				    f[x - 1][y].setParent(f[x][y]);
 							q.enqueue(f[x - 1][y]);
-							f[x - 1][y].setVisited(true);
+				    f[x - 1][y].setVisited(true);
 						}
 					    if (y + 1 < m
-								&& (String.valueOf(
-									f[x][y + 1].getVal(
-									)).toCharArray()[0] == '.'
-									|| String.valueOf(
-										f[x][y + 1].getVal(
-										)).toCharArray(
-										)[0] == 'E')
-								&& !f[x][y + 1].getVisited()) {
-							f[x][y + 1].setParent(f[x][y]);
+						&& (String.valueOf(
+							f[x][y + 1].getVal(
+						)).toCharArray()[0] == '.'
+							|| String.valueOf(
+							f[x][y + 1].getVal(
+							)).toCharArray(
+							)[0] == 'E')
+					&& !f[x][y + 1].getVisited()) {
+					f[x][y + 1].setParent(f[x][y]);
 							q.enqueue(f[x][y + 1]);
-							f[x][y + 1].setVisited(true);
+					f[x][y + 1].setVisited(true);
 						}
-						if (x + 1 < n &&
-								(String.valueOf(
-								f[x + 1][y].getVal(
-								)).toCharArray()[0] == '.'
-										|| String.valueOf(
-												f[x + 1][y].getVal(
-												)).toCharArray(
-												)[0] == 'E')
-								&& !f[x + 1][y].getVisited()) {
-							f[x + 1][y].setParent(f[x][y]);
+						if (x + 1 < n
+				&& (String.valueOf(
+				f[x + 1][y].getVal())
+				.toCharArray()[0] == '.'
+			  || String.valueOf(f[x + 1][y].getVal(
+			 )).toCharArray()[0] == 'E')
+					&& !f[x + 1][y].getVisited()) {
+					f[x + 1][y].setParent(f[x][y]);
 							q.enqueue(f[x + 1][y]);
-							f[x + 1][y].setVisited(true);
+					f[x + 1][y].setVisited(true);
 						}
 						if (y - 1 >= 0
-								&& (String.valueOf(
-									f[x][y - 1].getVal(
-									)).toCharArray()[0] == '.'
-											|| String.valueOf(
-													f[x][y - 1].getVal(
-													)).toCharArray(
-													)[0] == 'E')
-								&& !f[x][y - 1].getVisited()) {
-							    f[x][y - 1].setParent(f[x][y]);
-								q.enqueue(f[x][y - 1]);
-								f[x][y - 1].setVisited(true);
+						&& (String.valueOf(
+						f[x][y - 1].getVal(
+						)).toCharArray()[0] == '.'
+						|| String.valueOf(
+						f[x][y - 1].getVal(
+						)).toCharArray(
+						)[0] == 'E')
+						&& !f[x][y - 1].getVisited()) {
+					    f[x][y - 1].setParent(f[x][y]);
+						q.enqueue(f[x][y - 1]);
+						f[x][y - 1].setVisited(true);
 							}
 						}
-					
-				}
-				if (Character.valueOf((char)
-						f[t][r].getVal()) == 'E') {
-					gg = false;
 				}
 			}
 		}
@@ -178,13 +162,13 @@ public class MazeSolver implements IMazeSolver {
 		}
 		s.enqueue(t);
 		int size = s.size();
-		int[][] n = new int [size][2];
+		int[][] nn = new int[size][2];
 		for (int a = size - 1; a >= 0; a--) {
 			Node v = (Node) s.dequeue();
-			n[a][0] = Integer.valueOf(String.valueOf(v.getx()));
-			n[a][1] = Integer.valueOf(String.valueOf(v.gety()));
+			nn[a][0] = Integer.valueOf(String.valueOf(v.getx()));
+			nn[a][1] = Integer.valueOf(String.valueOf(v.gety()));
 		}
-		return n;
+		return nn;
 	}
 	/**
 	 * check.
@@ -211,25 +195,20 @@ public class MazeSolver implements IMazeSolver {
 			if (s.hasNext()) {
 				n = s.nextInt();
 				m = s.nextInt();
-			}	
+			}
 			if (s.hasNext()) {
 				int you = 0, me = 0;
 				for (int j = 0; j < n; j++) {
 					zew = s.next();
-					int vv = 0;
 					for (int k = 0; k < m; k++) {
 						h.append(zew.charAt(k));
 						if (Character.valueOf(
-							zew.charAt(k)).equals('S')) {
+					zew.charAt(k)).equals('S')) {
 							you++;
-						} else if (Character.valueOf(
-								zew.charAt(k)).equals('E')) {
+					} else if (Character.valueOf(
+					zew.charAt(k)).equals('E')) {
 							me = 1;
-						} 
-						vv = k + 1;
-					}
-					if (vv < zew.length()) {
-						throw new RuntimeException();
+						}
 					}
 				}
 				if (me == 0 || you > 1) {
@@ -238,12 +217,10 @@ public class MazeSolver implements IMazeSolver {
 			} else {
 				throw new RuntimeException();
 			}
-			
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			throw new RuntimeException();
 		}
-		Node [][] f = new Node[n][m];
+		Node[][] f = new Node[n][m];
 		int b = 0;
 		for (int y = 0; y < n; y++) {
 			for (int z = 0; z < m; z++) {
@@ -253,9 +230,8 @@ public class MazeSolver implements IMazeSolver {
 				b++;
 			}
 		}
-		boolean p = true;
 		int fx = 0, fy = 0, kaz = 0;
-		boolean d = true;
+		boolean d = true, p = true;
 		for (int t = 0; t < n; t++) {
 			for (int r = 0; r < m; r++) {
 				if (Character.valueOf((char)
@@ -270,71 +246,60 @@ public class MazeSolver implements IMazeSolver {
 					    int x = Integer.valueOf(
 					    String.valueOf(temp.getx()));
 					    int y = Integer.valueOf(
-							    String.valueOf(temp.gety()));
+					String.valueOf(temp.gety()));
 					    if (String.valueOf(
-								f[x][y].getVal(
-								)).toCharArray(
-									)[0] == 'E') {
+				f[x][y].getVal()).toCharArray(
+						)[0] == 'E') {
 					    	p = false;
 					    	fx = Integer.valueOf(
 					    	String.valueOf(f[x][y].getx()));
 					    	fy = Integer.valueOf(
-							    	String.valueOf(f[x][y].gety()));
+					String.valueOf(f[x][y].gety()));
 					    	break;
 					    }
 					    if (x - 1 >= 0
-								&& (String.valueOf(
-								f[x - 1][y].getVal(
-								)).toCharArray()[0] == '.'
-								|| String.valueOf(
-										f[x - 1][y].getVal(
-										)).toCharArray(
-										)[0] == 'E')
-								&& !f[x - 1][y].getVisited()) {
-							f[x - 1][y].setParent(f[x][y]);
-							q.push(f[x - 1][y]);
-							f[x - 1][y].setVisited(true);
+						&& (String.valueOf(
+						f[x - 1][y].getVal()).
+					toCharArray()[0] == '.'
+					|| String.valueOf(f[x - 1][y].getVal(
+				   )).toCharArray()[0] == 'E')
+				 && !f[x - 1][y].getVisited()) {
+				f[x - 1][y].setParent(f[x][y]);
+						q.push(f[x - 1][y]);
+					f[x - 1][y].setVisited(true);
 						} else if (y + 1 < m
-								&& (String.valueOf(
-									f[x][y + 1].getVal(
-									)).toCharArray()[0] == '.'
-									|| String.valueOf(
-										f[x][y + 1].getVal(
-										)).toCharArray(
-										)[0] == 'E')
-								&& !f[x][y + 1].getVisited()) {
-							f[x][y + 1].setParent(f[x][y]);
+					&& (String.valueOf(
+					f[x][y + 1].getVal(
+					)).toCharArray()[0] == '.'
+					|| String.valueOf(f[x][y + 1].getVal(
+					)).toCharArray()[0] == 'E')
+					&& !f[x][y + 1].getVisited()) {
+				   f[x][y + 1].setParent(f[x][y]);
 							q.push(f[x][y + 1]);
-							f[x][y + 1].setVisited(true);
-						} else if (x + 1 < n &&
-								(String.valueOf(
-								f[x + 1][y].getVal(
-								)).toCharArray()[0] == '.'
-										|| String.valueOf(
-												f[x + 1][y].getVal(
-												)).toCharArray(
-												)[0] == 'E')
-								&& !f[x + 1][y].getVisited()) {
-							f[x + 1][y].setParent(f[x][y]);
-							q.push(f[x + 1][y]);
-							f[x + 1][y].setVisited(true);
-						} else if (y - 1 >= 0
-								&& (String.valueOf(
-									f[x][y - 1].getVal(
-									)).toCharArray()[0] == '.'
-											|| String.valueOf(
-													f[x][y - 1].getVal(
-													)).toCharArray(
-													)[0] == 'E')
-								&& !f[x][y - 1].getVisited()) {
-							    f[x][y - 1].setParent(f[x][y]);
-								q.push(f[x][y - 1]);
-								f[x][y - 1].setVisited(true);
+				f[x][y + 1].setVisited(true);
+						} else if (x + 1 < n
+					&& (String.valueOf(f[x + 1][y].getVal(
+					)).toCharArray()[0] == '.'
+					|| String.valueOf(f[x + 1][y].getVal(
+					)).toCharArray()[0] == 'E')
+					&& !f[x + 1][y].getVisited()) {
+				f[x + 1][y].setParent(f[x][y]);
+					q.push(f[x + 1][y]);
+				f[x + 1][y].setVisited(true);
+				} else if (y - 1 >= 0
+				&& (String.valueOf(
+				f[x][y - 1].getVal(
+				)).toCharArray()[0] == '.'
+				|| String.valueOf(f[x][y - 1].getVal(
+				)).toCharArray()[0] == 'E')
+				&& !f[x][y - 1].getVisited()) {
+			    f[x][y - 1].setParent(f[x][y]);
+				q.push(f[x][y - 1]);
+				f[x][y - 1].setVisited(true);
 							} else {
 								q.pop();
 							}
-						} 
-					
+						}
 				}
 			}
 		}
@@ -353,12 +318,12 @@ public class MazeSolver implements IMazeSolver {
 		}
 		s.push(t);
 		int size = s.size();
-		int[][] n = new int [size][2];
+		int[][] nn = new int[size][2];
 		for (int a = 0; a < size; a++) {
 			Node v = (Node) s.pop();
-			n[a][0] = Integer.valueOf(String.valueOf(v.getx()));
-			n[a][1] = Integer.valueOf(String.valueOf(v.gety()));
+			nn[a][0] = Integer.valueOf(String.valueOf(v.getx()));
+			nn[a][1] = Integer.valueOf(String.valueOf(v.gety()));
 		}
-		return n;	}
+		return nn;	}
 
 }
